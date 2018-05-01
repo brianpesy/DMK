52a882b5d2d9de8ca4bc17c057286aabf7967be7
Brian Sy <brianpesy@gmail.com>
2018-05-01 14:46:22 +0800

    GET functionality trial

--- DMK/DMK/ViewController.swift
+++ DMK/DMK/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var tblJSON: UITableView!
+    var arrRes = [[String:AnyObject]]() //Array of dictionary
+    
     override func viewDidLoad() {
+        //As the app loads, it does these.
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("hi\n")
+        Alamofire.request("https://api.androidhive.info/contacts/").responseJSON { (responseData) -> Void in //Like our get request
+            if((responseData.result.value) != nil) {
+                let swiftyJsonVar = JSON(responseData.result.value!)
+                print(swiftyJsonVar)
+                print("yeet")
+                
+                if let resData = swiftyJsonVar["contacts"].arrayObject { //We can use this in order to get the data from the server.
+                    self.arrRes = resData as! [[String:AnyObject]]
+                    print("in")
+                    print(resData)
+                }
+                if self.arrRes.count > 0 {
+                 //   self.tblJSON.reloadData() //error here? Unexpectedly found nil.
+                }
+            }
+        }
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAtIndexPath indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "jsonCell")!
+        var dict = arrRes[(indexPath as NSIndexPath).row]
+        cell.textLabel?.text = dict["name"] as? String
+        cell.detailTextLabel?.text = dict["email"] as? String
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrRes.count
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
